1a527eb0a2cfbe4b176a8abd3f867727313f4607
0102johnny <rt2824.recruit.1022@gmail.com>
2022-10-01 21:46:49 +0900

    Counterモデルのインスタンスのプロパティcountが0の時にMinus Buttonをタップ、またはcountが10の時にPlus ButtonをタップするとAlertが表示される処理の実装

--- PracticeProgressView/ViewController.swift
+++ PracticeProgressView/ViewController.swift
@@ -7,17 +7,31 @@
 
 import UIKit
 
+private enum AlertManager {
+    static let alertTitile = "エラー"
+    static let alertMessage = "0または10以上の値を取り扱うことはできません"
+    static let alertActionTitle = "OK"
+}
+
 final class ViewController: UIViewController {
     @IBOutlet private weak var countLabel: UILabel!
 
     @IBOutlet private weak var countProgressView: UIProgressView!
 
     @IBAction private func didTapPlusButton(_ sender: Any) {
+        guard counter.count < 10 else {
+            showAlert()
+            return
+        }
         counter.increment()
         countProgressView.setProgress(countProgressView.progress + 0.1, animated: true)
     }
 
     @IBAction private func didTapMinusButton(_ sender: Any) {
+        guard counter.count > 0 else {
+            showAlert()
+            return
+        }
         counter.decrement()
         countProgressView.setProgress(countProgressView.progress - 0.1, animated: true)
     }
@@ -36,6 +50,17 @@ final class ViewController: UIViewController {
 
     private func updateCountLabel() {
         countLabel.text = String(counter.count) + " / " + "10"
+    }
+
+    private func showAlert() {
+        let alertController = generateAlert()
+        present(alertController, animated: true)
+    }
+
+    private func generateAlert() -> UIAlertController {
+        let alertController = UIAlertController(title: AlertManager.alertTitile, message: AlertManager.alertMessage, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: AlertManager.alertActionTitle, style: .default))
+        return alertController
     }
 }
 
